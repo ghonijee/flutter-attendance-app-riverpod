4ede456e02f2f5c2e9fcd4dceec2369bdff765a9
Yunus Floo <yunus@floo.id>
2022-09-17 16:16:42 +0700

    update UI

--- lib/app/user_attendance/screens/attendance_screen.dart
+++ lib/app/user_attendance/screens/attendance_screen.dart
@@ -46,99 +46,104 @@ class AttendanceScreen extends HookConsumerWidget {
     var screen = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Presensi",
+      body: Container(
+        constraints: BoxConstraints(
+          minHeight: screen.height,
+          maxWidth: screen.width,
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingMasterlocationScreen()));
-            },
-            icon: const Icon(Icons.settings),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: screen.width,
-                padding: const EdgeInsets.fromLTRB(16, 24, 16, 24),
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      "Hi, Selamat Datang",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+        // height: screen.height,
+        // width: screen.width,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: screen.width,
+              padding: const EdgeInsets.fromLTRB(16, 24, 16, 24),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingMasterlocationScreen()));
+                      },
+                      icon: Icon(
+                        Icons.settings,
+                        color: Theme.of(context).colorScheme.primary,
                       ),
                     ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      "Silakan check in kehadiranmu hari ini!",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-                color: Colors.white,
-                child: TableCalendar<AttendanceModel>(
-                  headerStyle: const HeaderStyle(
-                    titleCentered: true,
-                    formatButtonVisible: false,
                   ),
-                  focusedDay: focusDay.value,
-                  firstDay: DateTime.utc(2010, 10, 16),
-                  lastDay: DateTime.utc(2030, 3, 14),
-                  weekendDays: const [DateTime.saturday, DateTime.sunday],
-                  startingDayOfWeek: StartingDayOfWeek.monday,
-                  onDaySelected: (selectedDay, focusedDay) async {
-                    focusDay.value = selectedDay;
-                    await notifier.changeDate(focusDay.value);
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    "Hi, Selamat Datang",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Silakan check in kehadiranmu hari ini!",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              color: Colors.white,
+              child: TableCalendar<AttendanceModel>(
+                headerStyle: const HeaderStyle(
+                  titleCentered: true,
+                  formatButtonVisible: false,
+                ),
+                focusedDay: focusDay.value,
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                weekendDays: const [DateTime.saturday, DateTime.sunday],
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                onDaySelected: (selectedDay, focusedDay) async {
+                  focusDay.value = selectedDay;
+                  await notifier.changeDate(focusDay.value);
+                },
+                selectedDayPredicate: (day) => isSameDay(day, focusDay.value),
+                enabledDayPredicate: (day) {
+                  return day.day <= DateTime.now().day;
+                },
+                calendarBuilders: CalendarBuilders(
+                  dowBuilder: (context, day) {
+                    if (day.weekday == DateTime.saturday || day.weekday == DateTime.sunday) {
+                      final text = DateFormat.E().format(day);
+                      return Center(
+                        child: Text(
+                          text,
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      );
+                    }
                   },
-                  selectedDayPredicate: (day) => isSameDay(day, focusDay.value),
-                  enabledDayPredicate: (day) {
-                    return day.day <= DateTime.now().day;
-                  },
-                  calendarBuilders: CalendarBuilders(
-                    dowBuilder: (context, day) {
-                      if (day.weekday == DateTime.saturday || day.weekday == DateTime.sunday) {
-                        final text = DateFormat.E().format(day);
-                        return Center(
-                          child: Text(
-                            text,
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        );
-                      }
-                    },
 
-                    // defaultBuilder: (context, day, focusedDay) {},
-                  ),
+                  // defaultBuilder: (context, day, focusedDay) {},
                 ),
               ),
-              Container(
+            ),
+            Expanded(
+              child: Container(
                 padding: const EdgeInsets.fromLTRB(16, 20, 16, 20),
                 color: Colors.white,
                 width: screen.width,
@@ -287,9 +292,9 @@ class AttendanceScreen extends HookConsumerWidget {
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
